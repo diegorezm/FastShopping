0d0416278b1ee42989203d8ba175751f7b2341f0
diegorezm <dmourarezende@gmail.com>
2026-03-03 12:15:56 -0300

    enable scheduling

--- src/main/java/com/api/FastShopping/FastShoppingApplication.java
+++ src/main/java/com/api/FastShopping/FastShoppingApplication.java
@@ -4,17 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
-import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 @EnableTransactionManagement
+@EnableScheduling
 @EnableCaching
 public class FastShoppingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FastShoppingApplication.class, args);
-	}
+    static void main(String[] args) {
+        SpringApplication.run(FastShoppingApplication.class, args);
+    }
 
 }
